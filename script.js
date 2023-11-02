d6a06febcf24d9f9e27b9ed3a8925d6f80090c7a
cybertecna <mjkopacki@gmail.com>
2023-11-02 20:20:18 +0700

    Consistency changes, changed "" to ''

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
-const searchBtn = document.querySelector("#search-btn")
-const userInput = document.querySelector("#user-input")
-const searchForm = document.querySelector("#search-form")
-const searchedMovies = document.querySelector("#searched-movies")
-const exploreMovies = document.querySelector("#explore")
+const searchBtn = document.querySelector('#search-btn')
+const userInput = document.querySelector('#user-input')
+const searchForm = document.querySelector('#search-form')
+const searchedMovies = document.querySelector('#searched-movies')
+const exploreMovies = document.querySelector('#explore')
 let html = []
-const errorPopUp = document.querySelector("#pop-up")
+const errorPopUp = document.querySelector('#pop-up')
 const watchlistArray = []
 
 searchForm.addEventListener('submit', function(e) {
     e.preventDefault()
     searchMovies()
-    userInput.value = ""
+    userInput.value = ''
 })
 
 async function searchMovies() {
@@ -21,7 +21,7 @@ async function searchMovies() {
         errorPopUp.style.display = 'flex'
     
     } else if(moviesArray) {
-        exploreMovies.style.display="none"
+        exploreMovies.style.display= 'none'
         html = []
         for(let movie of moviesArray.Search) {
             generateMovies(movie.Title)
@@ -35,18 +35,21 @@ function generateMovies(movieTitle) {
     .then(res => res.json())
     .then(movieData => {
         html.push(`   
-            <div class="movie-data">
-                <img src=${movieData.Poster} class="poster" alt='Official poster of the movie ${movieData.Title}'>
-                <div class="movie-info">
-                    <h2 class="movie-title">${movieData.Title} <span class="rating"><img src='./images/star.png' class='star' alt='A star icon'>
+            <div class='movie-data'>
+                <img src=${movieData.Poster} class='poster' alt='Official poster of the movie ${movieData.Title}'>
+                <div class='movie-info'>
+                    <h2 class='movie-title'>${movieData.Title} 
+                    <span class='rating'> 
+                    <img src='./images/star.png' class='star' alt='A star icon'>
                     ${movieData.imdbRating}</span></h2>
                     <div class='runtime-genre-watchlist''>
                         <p><i>${movieData.Runtime}</i></p>
                         <p>${movieData.Genre}</p>
-                        <p class="add-to-watchlist" id ='${movieData.imdbID}' data-title='${movieData.Title}'><img src='./images/plus-symbol-button.png' class='icon' alt='A plus symbol icon'> Add to watchlist</p>
+                        <p class='add-to-watchlist' id ='${movieData.imdbID}' data-title='${movieData.Title}'>
+                        <img src='./images/plus-symbol-button.png' class='icon' alt='A plus symbol icon'> Add to watchlist</p>
                     </div>
                     <p>${movieData.Plot}</p>
-                    <p class="additional-info">Directed by: <i>${movieData.Director}</i><br>
+                    <p class='additional-info'>Directed by: <i>${movieData.Director}</i><br>
                        Written by: <i>${movieData.Writer}</i><br>
                        Actors: <i>${movieData.Actors}</i><br>
                        Country: ${movieData.Country}<br>
@@ -59,7 +62,7 @@ function generateMovies(movieTitle) {
 `
         )
     })
-    .then(() => searchedMovies.innerHTML = html.join("") )
+    .then(() => searchedMovies.innerHTML = html.join('') )
 
 }
 
@@ -72,7 +75,8 @@ document.addEventListener('click', function(e) {
         watchlistArray.push(e.target.dataset.title)
         localStorage.setItem('watchlist',JSON.stringify(watchlistArray))
         console.log(e.target.id)
-        document.getElementById(e.target.id).innerHTML = `<p class='added-to-the-watchlist'><img src='./images/check.png' alt='A check icon' class='icon'>
+        document.getElementById(e.target.id).innerHTML = `<p class='added-to-the-watchlist'>
+                                                            <img src='./images/check.png' alt='A check icon' class='icon'>
                                                             Added to the watchlist</p>`
     }
 })
--- scriptwatchlist.js
+++ scriptwatchlist.js
@@ -20,10 +20,10 @@ function generateWatchlist(movieTitle) {
         .then(res => res.json())
         .then(movieData => {
             watchlistHtml.push(`   
-                <div class="movie-data">
-                    <img src=${movieData.Poster} class="poster" alt='Official poster of the movie ${movieData.Title}'>
-                    <div class="movie-info">
-                        <h2 class="movie-title">${movieData.Title} <span class="rating"><img src='./images/star.png' class='star'
+                <div class='movie-data'>
+                    <img src=${movieData.Poster} class='poster' alt='Official poster of the movie ${movieData.Title}'>
+                    <div class='movie-info'>
+                        <h2 class='movie-title'>${movieData.Title} <span class='rating'><img src='./images/star.png' class='star'
                         alt='A star icon'>
                         ${movieData.imdbRating}</span></h2>
                         <div class='runtime-genre-watchlist''>
@@ -31,20 +31,18 @@ function generateWatchlist(movieTitle) {
                             <p>${movieData.Genre}</p>
                         </div>
                         <p>${movieData.Plot}</p>
-                        <p class="additional-info">Directed by: <i>${movieData.Director}</i><br>
+                        <p class='additional-info'>Directed by: <i>${movieData.Director}</i><br>
                            Written by: <i>${movieData.Writer}</i><br>
                            Actors: <i>${movieData.Actors}</i><br>
                            Country: ${movieData.Country}<br>
                            Box Office: ${movieData.BoxOffice}
                            </p>
                     </div>
-    
-    
                 </div>
     `
             )
         })
-        .then(() => userWatchlist.innerHTML = watchlistHtml.join("") )
+        .then(() => userWatchlist.innerHTML = watchlistHtml.join('') )
     
     }
     
